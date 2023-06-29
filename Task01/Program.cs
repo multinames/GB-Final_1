3a4650b91f788d136ae2482290e765fb76ebb904
Sergey Skuridin <skuridin@gmail.com>
2023-06-29 16:47:26 +0300

    add block diagram file

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,64 +1,43 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо 
 // равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
 
-// Создание и заполнение массива
-string[] arr1 = new string[3];
-arr1[0] = "abc1";
-arr1[1] = "!%11";
-arr1[2] = "ghi2";
+Console.Clear();
 
-// Создание переменной счетчика
-// int count_arr = 0;
+// Создание и заполнение исходного массива
+string[] arr1 = new string[4];
+arr1[0] = "abc1-";
+arr1[1] = "2";
+arr1[2] = "!ghi2";
+arr1[3] = ":-)";
 
-/* void PrintArray(string[] arr)
-// Вывод всех элементов массива
-{
-    Console.Write("[");
-    for(int i=0; i<arr.Length;i++) 
-    {
-           Console.Write($"\"{arr[i]}\", ");
-    }
-    Console.Write("]");
-}
- */
 int CalcSize(string[] arr1, int count_arr)
-// Подсчет строк в массиве совпадающих по количеству символов
+// Метод для подсчета строк в массиве удовлетворяющих условию
 {
 for (int i = 0; i < arr1.Count(); i++)
         if (arr1[i].Length <= 3) {
         count_arr += 1;
-        // Console.Write($"{arr1[i].Count()} ");
         }
-    // Console.WriteLine("count " + count_arr);
     return count_arr;
 }        
 
-// Перемещение нужных строк в новый массив
-
-int count = CalcSize(arr1,0);
-// Console.WriteLine(count);
-
-string[] arr2 = new string[count];
-
-void ToNewArray(string[] arr1, string[] arr2, int j) 
-// Добавление строк менее трех смиволов в новый массив
+void ToNewArray(string[] arr1, string[] arr2, int count_arr) 
+// Метод добавления строк, содержащих менее трех символов, в новый массив
 {
 for (int i = 0; i < arr1.Count(); i++)    
         if (arr1[i].Length <= 3) 
         {
-        arr2[j]=arr1[i];
-        j+=1;
-       // Console.WriteLine("j = "+j);
-   //    Console.Write($"{arr2[j-1]} "); 
-     //   Console.Write($"{String.Join(" ",arr2)} "); 
+        arr2[count_arr]=arr1[i];
+        count_arr+=1;
         }
         
 }
 
-//PrintArray(arr1);
+// Создание нового массива необходимого размера после подсчета удовлетворяющих условию строк в исходном массиве
+string[] arr2 = new string[CalcSize(arr1,0)];
 
-Console.Write($"[{String.Join(", ", arr1)}] ->");
 
 ToNewArray(arr1, arr2, 0);
 
+//Вывод исходного и нового массивов
+Console.Write($"[{String.Join(", ", arr1)}] ->");
 Console.Write($" [{String.Join(", ", arr2)}]");
